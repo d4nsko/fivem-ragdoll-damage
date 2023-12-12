d0b290ca69c60d2ebe3f7873f06ef50541408374
d4nsko <76801103+d4nsko@users.noreply.github.com>
2023-12-12 11:08:15 +0100

    updated fxmanifest

---
+++ fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version  	'cerulean'
+game 			    'gta5'
+lua54			    'yes'
+
+author        'd4nsko'
+description   'a FiveM resource that makes a player ragdoll when hit in certain bones.'
+
+--
+
+client_scripts {
+    'client.lua'
+}
+
+
+server_scripts {
+    ''
+}
